912e6375a6544186ddb507933b2b462d24a849cb
Administrator <admin@example.com>
2019-03-13 15:57:09 -0500

    exercise 7 with drills

---
+++ ex7.rb
@@ -0,0 +1,45 @@
+puts "Marry had a little lamb."
+# string is printed out
+puts "Its fleece was white as #{'snow'}."
+# #{} literal is evaluated into the string and then the string is printed out
+# is it called an interpolation operator? 
+puts "And everywhere that Mary went."
+# prints string
+puts "." * 10 # what'd that do?
+# prints period 10 times
+
+end1 = "C"
+# assigns value (a string) to variable
+end2 = "h"
+# assigns value (a string) to variable
+end3 = "e"
+# assigns value (a string) to variable
+end4 = "e"
+# assigns value (a string) to variable
+end5 = "s"
+# assigns value (a string) to variable
+end6 = "e"
+# assigns value (a string) to variable
+end7 = "B"
+# assigns value (a string) to variable
+end8 = "u"
+# assigns value (a string) to variable
+end9 = "r"
+# assigns value (a string) to variable
+end10 = "g"
+# assigns value (a string) to variable
+end11 = "e"
+# assigns value (a string) to variable
+end12 = "r"
+# assigns value (a string) to variable
+
+# watch that print vs. puts on this line what's it do? 
+puts end1 + end2 + end3 + end4 + end5 + end6 + end7 + end8 + end9 + end10 + end11 + end12
+puts end1 + end2 + end3 + end4 + end5 + end6 + end7 + end8 + end9 + end10 + end11 + end12
+
+# prints out concatination of variable values
+# print vs puts = both print out a string, but puts adds a new line after the execution
+# Above with print will return CheeseBurgerCheeseBurger
+# Above with puts will return 
+# CheeseBurger
+# CheeseBurger
