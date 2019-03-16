16f27bfe47f076ccc2295537d6a931e435e048cb
Administrator <admin@example.com>
2019-03-16 13:50:31 -0500

    exercise 10

---
+++ ex10.rb
@@ -0,0 +1,26 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+# Drill 3
+puts "Trying to figure out what \r does?"
+puts "That\'s weird. okay."
+puts "Gunna \ttab\ttab\ttab it in."
+puts "But we also have to try and escape the single \"quote\""
+puts "There are so many of \f these oh my goodness."
+puts "Oh I like \f that!"
+puts "There\'s also a \"\\a\" to figure out?"
+puts "What does it \a do?"
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+
