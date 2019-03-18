0f0655b379d982371b6b9d7b39f92245ed4e9d5a
Administrator <admin@example.com>
2019-03-18 12:31:38 -0500

    exercise 11 with drillz :tada:

---
+++ ex11.rb
@@ -0,0 +1,21 @@
+# print "How old are you? "
+# age = gets.chomp
+# print "How tall are you? "
+# height = gets.chomp
+# print "How much do you weigh? "
+# weight = gets.chomp
+
+# puts "So, youre #{age} old, #{height} tall, and #{weight} heavy."
+
+print "What is your favorite animal? "
+animal = gets.chomp
+print "What is your favorite color? "
+color = gets.chomp
+print "What is your favorite food? "
+food = gets.chomp
+
+puts "I like that a #{animal} is your favorite animal! Maybe you could find a #{color} colored one that likes to eat #{food}."
+
+# "gets.chomp" meaning:
+# "gets" is a method that asks the user to input something.
+# "chomp" is a method that removes the blank line that is automatically created by "gets" after the input.
